a4060dce2c91ca4d2c6a42d611eeb67ac8b4fda7
joranmulderij <joran.mulderij@gmail.com>
2022-01-07 11:32:02 +0500

    Solve null safety related type error

--- lib/src/helpers/converter.dart
+++ lib/src/helpers/converter.dart
@@ -1,9 +1,10 @@
 /// Simplifies getting and parsing the enum values.
 class Converter {
   /// Get enum easily from a string
-  static T enumFromJson<T>(List<T> values, String? json) => values.firstWhere(
-      (it) => enumToJson(it)?.toLowerCase() == json?.toLowerCase(),
-      orElse: () => values.first);
+  static T enumFromJson<T>(List<T> values, String? json) =>
+      values.firstWhere(
+          (it) => enumToJson(it)?.toLowerCase() == json?.toLowerCase()) ??
+      values.first;
 
   /// Transform enum to json
   static String? enumToJson<T>(T? value) =>
